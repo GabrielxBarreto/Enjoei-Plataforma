42e4e080d8600e613272c41ccf1c6fc00fef347f
Vítor Dourado <114265954+MASTERCARS@users.noreply.github.com>
2023-05-17 22:54:51 -0300

    adicionei os arquivos index php e estilo css
    ajuste na pagina inicial, html e css

---
+++ pagina1/index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Venda de produtos</title>
+    <link rel="stylesheet" href="estilo.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <div class="logo">
+                <img src="logo/aqui/imagem.jpg" alt="Logo da empresa">
+            </div>
+            <form class="search-form">
+                <input type="text" placeholder="Pesquisar">
+                <button type="submit">Buscar</button>
+                <span>BRESHOP</span>
+            </form>
+        </nav>
+    </header>
+	
+	<h1> BANNER AQUI[]</h1><br>
+    
+	<section> <div class="product">
+            <h3>Blusas</h3>
+        </div>
+        <div class="product">
+            <h3>Calças</h3>
+            </div>
+    <div class="product">
+        <h3>Sapatos</h3>
+    </div>
+    <div class="product">
+        <h3>Acessórios</h3>
+    </div></section>
+            
+    <main>
+    
+<br> <br>
+    <div class="desconto">
+        <p>DESCONTOS</p>
+    </div>
+   
+    
+    <div class="grid-container">
+	
+            <div class="grid-item1">quadro1</div>
+            <div class="grid-item2">quadro 2</div>
+            <div class="grid-item3">quadro 3</div>
+            <div class="grid-item4">quador4</div>
+            <div class="grid-item5">quadro5</div>
+            <div class="grid-item6">quadro6</div>
+            <div class="grid-item7">quadro 7</div>
+            <div class="grid-item8">quadro8</div>
+            <div class="grid-item9">quador9</div>
+            
+
+			
+        </div>
+
+        <div class="marca">
+            <div class="marca-elementos">
+                <h3>NIKE</h3>
+            </div>
+        </div>
+
+
+        <div class="maisvendidos">
+        <p>MAIS VENDIDOS</p>
+    </div>
+   
+
+
+
+
+</main>
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+<footer>
+    <p>© 2023 Venda de produtos. Todos os direitos reservados.</p>
+</footer>
+</body>
+</html>
