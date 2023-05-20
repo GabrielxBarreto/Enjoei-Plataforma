f25bb7094406b0d34c66f68f6c74e88509b31e02
GabrielxBarreto <gabrieloakbarreto@gmail.com>
2023-05-19 23:09:05 -0300

    segundo container
    adicionado o segundo container do display grid

--- pagina1/index.php
+++ pagina1/index.php
@@ -1,72 +1,79 @@
 <?php
-    require "../estoque/estoque_Acessorios.php";
-    require "../estoque/estoque_Blusas.php";
-    require "../estoque/estoque_Calcas.php";
-    require "../estoque/estoque_Sapatos.php";
+require "../estoque/estoque_Acessorios.php";
+require "../estoque/estoque_Blusas.php";
+require "../estoque/estoque_Calcas.php";
+require "../estoque/estoque_Sapatos.php";
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Venda de produtos</title>
     <link rel="stylesheet" href="estilo.css">
 </head>
+
 <body>
     <a href="/estoque/estoque_Acessorios.php"></a>
     <header>
         <nav>
             <div class="logo">
-                <img src="logo/aqui/imagem.jpg" alt="Logo da empresa">
+                <img id="logo" src="../breshoplogo.png" alt="Logo da empresa">
             </div>
             <form class="search-form">
                 <input type="text" placeholder="Pesquisar">
                 <button type="submit">Buscar</button>
-                <span>BRESHOP</span>
             </form>
+            <img id="conta" src="../usuarioicone.png" alt="userIcon" srcset="">
+            <img id="conta" src="../simbolocompras.png" alt="userIcon" srcset="">
         </nav>
     </header>
-	
-	<h1> BANNER AQUI[]</h1><br>
-    
-	<section> <div class="product">
+
+    <div class="banner">
+        <img id="banner_img" src="https://imagensemoldes.com.br/wp-content/uploads/2020/04/Ilustra%C3%A7%C3%A3o-50-de-Desconto-PNG.png" alt="banner">
+    </div><br>
+
+    <section>
+        <div class="product">
             <h3>Blusas</h3>
         </div>
         <div class="product">
             <h3>Calças</h3>
-            </div>
-    <div class="product">
-        <h3>Sapatos</h3>
-    </div>
-    <div class="product">
-        <h3>Acessórios</h3>
-    </div></section>
-            
+        </div>
+        <div class="product">
+            <h3>Sapatos</h3>
+        </div>
+        <div class="product">
+            <h3>Acessórios</h3>
+        </div>
+    </section>
+
     <main>
-    
-<br> <br>
-    <div class="desconto">
-        <p>DESCONTOS</p>
-    </div>
-   
-    
-    <div class="grid-container">
-    <?php
+
+        <br> <br>
+        <div class="desconto">
+            <p>DESCONTOS</p>
+        </div>
+
+
+        <div class="grid-container">
+            <?php
             $i = 0;
-            foreach($acessorios as $key => $auxiliar_acessorios){
-                if($i <= 8){
-                    if($i == 3){
+            foreach ($acessorios as $key => $auxiliar_acessorios) {
+                if ($i <= 9) {
+                    if ($i == 3) {
                         echo "<div class='grid-item{$i}'><img width='450' height='450' src={$auxiliar_acessorios["img"]}></img></div>";
-                    }else if($i == 6){
+                    } else if ($i == 6) {
                         echo "<div class='grid-item{$i}'><img width='450' height='450' src={$auxiliar_acessorios["img"]}></img></div>";
-                    }else if($i == 8){
+                    } else if ($i == 8) {
                         echo "<div class='grid-item{$i}'><img width='300' height='470' src={$auxiliar_acessorios["img"]}></img></div>";
                     }
                     $i++;
-                echo "<div class='grid-item{$i}'><img width='200' height='200' src={$auxiliar_acessorios["img"]}></img></div>";
-                }else{
+                    echo "<div class='grid-item{$i}'><img width='200' height='200' src={$auxiliar_acessorios["img"]}></img></div>";
+                } else {
                     break;
-                }    
-            }    
-        ?>	
+                }
+            }
+            ?>
         </div>
 
         <div class="marca">
@@ -77,26 +84,44 @@
 
 
         <div class="maisvendidos">
-        <p>MAIS VENDIDOS</p>
-    </div>
-    <div class="container-vendidos"> 
-        <div class="item1">quadro 1 </div>
-        <div class="item2">quadro 2 </div>
-        <div class="item3">quadro 3* </div>
-        <div class="item4">quadro 4 </div>
-        <div class="item5">quadro 5 </div>
-        <div class="item6">quadro 6 </div>
-        <div class="item7">quadro 7 </div>
-    </div> 
-    <div class="recomendados"><p>RECOMENDADOS </p> </div>
+            <p>MAIS VENDIDOS</p>
+        </div>
+        <div class="container-vendidos">
+            <?php
+            $j = 0;
+            foreach ($blusas as $key => $aux_blusas) {
+                if ($j <= 6) {
+
+                    $j++;
+                    if ($j == 3) {
+                        echo "<div class='item{$j}'><img width ='330' height='450'src='{$aux_blusas['img']}'></div>";
+                    
+                    }else{
+                        echo "<div class='item{$j}'><img width='200' height='200' src='{$aux_blusas['img']}'></div>";
+                    }
+                    
+                } else {
+                    break;
+                }
+            }
+
+
+            ?>
+
+
+        </div>
+        <div class="recomendados">
+            <p>RECOMENDADOS </p>
+        </div>
 
 
 
 
-</main>
+    </main>
 
-<footer>
-    <p>© 2023 Venda de produtos. Todos os direitos reservados.</p>
-</footer>
+    <footer>
+        <p>© 2023 Venda de produtos. Todos os direitos reservados.</p>
+    </footer>
 </body>
+
 </html>
