b5856966521ba72a448a093518123c55d10b2ffd
raissa <132288310+raissaoo@users.noreply.github.com>
2023-05-22 22:09:01 -0300

    Add files via upload

---
+++ img_produtos/estoque_Calcas.php
@@ -0,0 +1,219 @@
+<?php
+$calcas =  [
+    [
+        "nome"=>"calça preta legging",
+        "preco"=>50.00,
+        "desconto" => 30.00,
+        "condicao"=>"usado",
+        "descricao" => "cintura: 29cm
+        comprimento: 89cm",
+        "pecasEmEstoque" => 1,
+        "img" => "../img_produtos/legging_preta.jpg",
+        "marca" => "não identificado"
+    ],
+    [
+        "nome"=>"calça rosa",
+        "preco"=>169.00,
+        "desconto" => 60.00,
+        "condicao"=>"usado",
+        "descricao" => "calça reta rosa, tamanho 44, thaise morais, nova, mesmo tecido nv - acetato + viscose",
+        "pecasEmEstoque" => 1,
+        "img" => "../img_produtos/calca_rosa.jpg",
+        "marca" => "thaise morais"
+    ],
+    [
+        "nome"=>"calça flare",
+        "preco"=>100.00,
+        "desconto" => 0,
+        "condicao"=>"usado",
+        "descricao" => "calça jeans, modelo flare, com abertura na barra, cós alto, tamanho 42. não foi usada.",
+        "pecasEmEstoque" => 1,
+        "img" => "../img_produtos/calca_flare.jpg",
+        "marca" => "não identificado"
+    ],
+    [
+        "nome"=>"calça feminina social pied poule",
+        "preco"=>180.00,
+        "desconto" => 130.00,
+        "condicao"=>"usado",
+        "descricao" => "em tecido de jacquard, possui 2 bolsos frontais e 2 posteriores funcionais, comprimento das pernas afunilado e detalhe em fenda na barra. apresenta desgaste no tecido (bolinhas) devido lavagens e algumas manchas escurecidas na nádega direita.
+        cintura 44cm/gancho frontal 29cm/quadril 54cm/coxa 29cm/comprimento 99cm/largura passador 5cm",
+        "pecasEmEstoque" => 1,
+        "img" => "../img_produtos/calca_femin_social.jpg",
+        "marca" => "cortelle"
+    ],
+    [
+        "nome"=>"calça jeans bordada amô amô",
+        "preco"=>475.00,
+        "desconto" => 0,
+        "condicao"=>"novo",
+        "descricao" => "calça jeans da farm, bordada com estampa colorida de corações e cós alto, tendo umã modelagem confortável
+        ela está em perfeito estado, nunca usada",
+        "pecasEmEstoque" => 3,
+        "img" => "../img_produtos/calca_jeans_bordada.jpg",
+        "marca" => "farm"
+    ],
+    [
+        "nome"=>"calça amarela floral flare ak urban",
+        "preco"=>71.00,
+        "desconto" => 30.00,
+        "condicao"=>"usado",
+        "descricao" => "cintura: 78cm
+        quadril: 98cm
+        comprimento: 104cm",
+        "pecasEmEstoque" => 1,
+        "img" => "../img_produtos/calca_amarela.jpg",
+        "marca" => "riachuelo"
+    ],
+    [
+        "nome"=>"mom violeta a.brand",
+        "preco"=>375.00,
+        "desconto" => 0,
+        "condicao"=>"usado",
+        "descricao" => "calça violeta em seda com bolsos, barra com amarração, fecho em colchetes e zíper e modelagem mom.",
+        "pecasEmEstoque" => 1,
+        "img" => "../img_produtos/calca_mom.jpg",
+        "marca" => "a.brand"
+    ],
+    [
+        "nome"=>"moletom calvin klein",
+        "preco"=>175.00,
+        "desconto" => 157.00,
+        "condicao"=>"usado",
+        "descricao" => "modelo de pernas curtas , comprado nos eua. usada .",
+        "pecasEmEstoque" => 1,
+        "img" => "../img_produtos/calca_moletom.jpg",
+        "marca" => "calvin klein"
+    ],
+    [
+        "nome"=>"calça azul jeans iorane",
+        "preco"=>489.00,
+        "desconto" => 0,
+        "condicao"=>"novo",
+        "descricao" => "cintura: 76cm
+        quadril: 92cm
+        gancho: 33cm
+        comprimento: 118cm",
+        "pecasEmEstoque" => 1,
+        "img" => "../img_produtos/calca_jeans_iorane.jpg",
+        "marca" => "iorane"
+    ],
+    [
+        "nome"=>"calça pantacour haes p",
+        "preco"=>120.00,
+        "desconto" => 91.00,
+        "condicao"=>"usado",
+        "descricao" => "calça pantacour haes p, cor cru, elástico largo e firme, medindo 70 cm sem esticar, abertura nas laterais, veste m tbm, usado 1x",
+        "pecasEmEstoque" => 1,
+        "img" => "../img_produtos/calca_pantacour_haes.jpg",
+        "marca" => "haes"
+    ],
+    [
+        "nome"=>"calça social alfaiataria",
+        "preco"=>66.00,
+        "desconto" => 0,
+        "condicao"=>"usado",
+        "descricao" => "calça social da marca invait preta com listras cinza da marca invait. dois bolsos na frente, fechamento botão e zíper. estado: usado em bom estado.",
+        "pecasEmEstoque" => 1,
+        "img" => "../img_produtos/calca_social.jpg",
+        "marca" => "invait"
+    ],
+    [
+        "nome"=>"calça jeans feminina diesel",
+        "preco"=>800.00,
+        "desconto" => 381.00,
+        "condicao"=>"usado",
+        "descricao" => "calça jeans feminina,cintura baixa,98% coton e 2% spandex,usada em perfeito estado.",
+        "pecasEmEstoque" => 1,
+        "img" => "../img_produtos/calca_jeans_diesel.jpg",
+        "marca" => "diesel"
+    ],
+    [
+        "nome"=>"calça em couro zara",
+        "preco"=>198.00,
+        "desconto" => 0,
+        "condicao"=>"usado",
+        "descricao" => "calça em couro marrom coleção nova inverno 2023 usei 2 x e ficou larguinha pra mim por isso desapego",
+        "pecasEmEstoque" => 1,
+        "img" => "../img_produtos/calca_couro_zara.jpg",
+        "marca" => "zara"
+    ],
+    [
+        "nome"=>"calça mostarda listrada talita kume",
+        "preco"=>99.00,
+        "desconto" => 0,
+        "condicao"=>"usado",
+        "descricao" => "cintura: 74cm
+        quadril: 90cm
+        comprimento: 98cm",
+        "pecasEmEstoque" => 1,
+        "img" => "../img_produtos/calca_mostarda.jpg",
+        "marca" => "talita kume"
+    ],
+    [
+        "nome"=>"calça vermelha floral moda india",
+        "preco"=>65.00,
+        "desconto" => 0,
+        "condicao"=>"novo",
+        "descricao" => "cintura: 40cm
+        comprimento: 104cm",
+        "pecasEmEstoque" => 30,
+        "img" => "../img_produtos/calca_vermelha.jpg",
+        "marca" => "moda índia"
+    ],
+    [
+        "nome"=>"calça jeans preta com listra prata.",
+        "preco"=>129.00,
+        "desconto" => 0,
+        "condicao"=>"novo",
+        "descricao" => "nova com etiqueta, queima de estoque. tamanhos disponíveis 36, 40 e 44.",
+        "pecasEmEstoque" => 20,
+        "img" => "../img_produtos/calca_jeans_preta.jpg",
+        "marca" => "trama jeans"
+    ],
+    [
+        "nome"=>"calça azul petróleo pantacourt nanaminze",
+        "preco"=>189.00,
+        "desconto" => 141.00,
+        "condicao"=>"novo",
+        "descricao" => "cintura: 76cm
+        comprimento: 86cm",
+        "pecasEmEstoque" => 26,
+        "img" => "../img_produtos/calca_azul.jpg",
+        "marca" => "nanaminze"
+    ],
+    [
+        "nome"=>"calça xadrez amarela",
+        "preco"=>30.00,
+        "desconto" => 25.00,
+        "condicao"=>"usado",
+        "descricao" => "calça com estampa xadrez na cor amarela, com zíperes para fechamento. não possui bolsos. tecido estica bem.
+        96% poliéster e 4% elastano",
+        "pecasEmEstoque" => 1,
+        "img" => "../img_produtos/calca_xadrez.jpg",
+        "marca" => "marisa"
+    ],
+    [
+        "nome"=>"calça jeans zara turquia",
+        "preco"=>79.00,
+        "desconto" => 46.00,
+        "condicao"=>"usado",
+        "descricao" => "cintura: 80
+        quadril: 98
+        comp: 93",
+        "pecasEmEstoque" => 1,
+        "img" => "../img_produtos/calca_jeans_zara.jpg",
+        "marca" => "zara"
+    ],
+    [
+        "nome"=>"calça cargo zara",
+        "preco"=>80.00,
+        "desconto" => 64.00,
+        "condicao"=>"usado",
+        "descricao" => "calça feminina da marca zara, modelo cargo, tamanho eur 42, usa 10, mex 32, veste 42 br. tecido 98% algodão e 2%elastano. medidas: cintura 92 cm, gancho 23.5 cm, quadril 100 cm, comprimento 98 cm, diâmetro da barra 32 cm, entrepernas 78 cm, diâmetro da coxa 59 cm. usada",
+        "pecasEmEstoque" => 1,
+        "img" => "../img_produtos/calca_cargo.jpg",
+        "marca" => "zara"
+    ],
+]
+?>
