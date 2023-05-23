669d63904c842b9c2812332170ae01a4f7369625
raissa <132288310+raissaoo@users.noreply.github.com>
2023-05-22 23:01:30 -0300

    Update estoqueGeral.php

--- estoque/estoqueGeral.php
+++ estoque/estoqueGeral.php
@@ -693,5 +693,188 @@ $estoqueGeral =  [
         "img" => "../img_produtos/calca_cargo.jpg",
         "marca" => "zara"
     ],
+    [
+        "nome"=>"Tênis Nike Air Force 1 Low",
+        "preco"=> 1199.99,
+        "desconto" => 1009.00,
+        "descricao" => "Tênis Casual Air Force 1 Em Couro Cor - Branco",
+        "pecasEmEstoque" => 245,
+        "img" => "../img_produtos/air_force1.avif",
+        "marca" => "nike"
+    ],
+    [
+        "nome"=>"Tênis Nike Air Max SC Masculino",
+        "preco"=> 359.99,
+        "desconto" => false,
+        "descricao"=>"ESTILO OLD SCHOOL. CONFORTO NEW SCHOOL.Com suas linhas simples, aparência de pista <br>tradicional e, claro, amortecimento de ar visível, o Nike Air Max SC <br>é o acabamento perfeito para qualquer estilo.",
+        "pecasEmEstoque" => 345,
+        "img" => "../img_produtos/air_max.webp",
+        "marca" => "nike"
+
+    ],
+    [
+        "nome"=>"Tênis Nike Court Legacy Masculino",
+        "preco"=> 299.99,
+        "desconto" => false,
+        "descricao"=>"tênis original, diretamente da marca destinaria.",
+        "pecasEmEstoque" => 528, 
+        "img" => "../img_produtos/tenis_court.jpg",
+        "marca" => "nike"
+    ],
+    [
+        "nome"=>"Tênis Nike Renew Ride 3 Masculino",
+        "preco"=> 319.99,
+        "desconto" => false,
+        "descricao"=>"tênis original, diretamente da marca destinaria.",
+        "pecasEmEstoque" => 220,
+        "img" => "../img_produtos/renew_ride.webp",
+        "marca" => "nike"
+    ],
+    [
+        "nome" => "Tênis Nike Air Force 1 React 40TH Unissex",
+        "preco"=> 999,99,
+        "desconto" => false,
+        "descricao"=>"tênis original, diretamente da marca destinaria.",
+        "pecasEmEstoque" => 150,
+        "img" => "../img_produtos/air_force1_react.webp",
+        "marca" => "nike"
+    ],
+    [
+        "nome" => "Lote De All Star Converse Chuck Taylor Usa 80s",
+        "preco"=> 50.00,
+        "desconto" => false,
+        "descricao"=>"Todos comprados nos anos 80nos EUA, foram usados, porém estão ainda em muito bom estado de conservação.",
+        "pecasEmEstoque" => 2,
+        "img" => "../img_produtos/allstar.webp",
+        "marca" => "converse"
+    ],
+    [
+        "nome" => "Tênis Oakley Modoc Low 2.0",
+        "preco"=> 100.00,
+        "desconto" => false,
+        "descricao"=>"pouco uso",
+        "pecasEmEstoque" => 1,
+        "img" => "../img_produtos/oakley.webp",
+        "marca" => "oakley"
+    ],
+    [
+        "nome" => "Tênis All Star Skull / Caveira Couro Sintético / Raro 40",
+        "preco"=> 100.00,
+        "desconto" => true,
+        "descricao"=>"Usado apenas 2 vezes.
+        Modelo raro!
+        Número 40",
+        "pecasEmEstoque" => 1,
+        "img" => "../img_produtos/allstar_skull.webp",
+        "marca" => "converse"
+    ],
+    [
+        "nome" => "Tenis adidas Kanadia Tr7 :nr. 38 A 43",
+        "preco"=> 80.00,
+        "desconto" => false,
+        "descricao"=>"CORES DISPONÍVEIS: Marinho com Celeste / Preto com Laranja / Preto com Verde / Amarelo com Preto",
+        "pecasEmEstoque" => 4,
+        "img" => "../img_produtos/adidas_kanadia.webp",
+        "marca" => "adidas"
+    ],
+    [
+        "nome" => "Tênis Puma Smash V2 - Preto+Branco",
+        "preco"=> 256.00,
+        "desconto" => false,
+        "descricao"=>"PUMA SMASH V2 WNS BDP",
+        "pecasEmEstoque" => 120,
+        "img" => "../img_produtos/puma_smash.webp",
+        "marca" => "puma"
+    ],
+    [
+        "nome" => "Tênis Olympikus Mood Masculino - Branco",
+        "preco"=> 119.99,
+        "desconto" => false,
+        "descricao"=>"Aposte no Tênis Casual Olympikus Mood 43294 Masculino e tenha o melhor em estilo, qualidade e conforto. Feito em material sintético, este tênis possui um visual clássico, com pespontos na lateral e lingueta em material têxtil para sair um pouco do óbvio. Além disso, ele é super confortável, graças ao seu interior acolchoado, palmilha em EVA revestido e ainda possui a tecnologia EVASENSE no solado.",
+        "pecasEmEstoque" => 300,
+        "img" => "../img_produtos/olympikus_mood.webp",
+        "marca" => "olympikus"
+    ],
+    [
+        "nome" => "Tênis Mizuno Cometa - Preto+Branco",
+        "preco"=> 175.99,
+        "desconto" => false,
+        "descricao"=>"Tênis com o formato e amortecimento certos podem fazer muita diferença na hora de correr na esteira, por exemplo, pois não permitirão torções ou outras consequências.",
+        "pecasEmEstoque" => 206,
+        "img" => "../img_produtos/mizuno_cometa.webp",
+        "marca" => "mizuno"
+    ],
+    [
+        "nome" => "Tênis Nike Downshifter 12 Feminino - Rosa+Branco",
+        "preco"=> 279.99,
+        "desconto" => false,
+        "descricao"=>"SUSTENTAÇÃO LEVE PARA JOGAR O DIA TODO.Seja brincando de pega-pega ou iniciando um novo esporte, as crianças se sentem mais confiantes com o novo Nike Downshifter 12.",
+        "pecasEmEstoque" => 204,
+        "img" => "../img_produtos/nike_downsshifter.webp",
+        "marca" => "nike"
+    ],
+    [
+        "nome" => "Tênis Beira Rio Nude",
+        "preco"=> 45.00,
+        "desconto" => false,
+        "descricao"=>"pouco uso",
+        "pecasEmEstoque" => 1,
+        "img" => "../img_produtos/beirario_nude.jpg",
+        "marca" => "beira rio"
+    ],
+    [
+        "nome" => "mocassim jeans tratorado oxford casual feminino lançamento",
+        "preco"=> 100.00,
+        "desconto" => 85.00,
+        "descricao"=>"Esteja pronta para adotar o sapato da vez: o mocassim! Ele saiu da sapateira masculina e foi correndo para a feminina com muito charme e sofisticação.",
+        "pecasEmEstoque" => 1,
+        "img" => "../img_produtos/mocassim_jeans.jpg",
+        "marca" => "mocassim"
+    ],
+    [
+        "nome" => "Tênis Air Jordan",
+        "preco"=> 79.00,
+        "desconto" => 49.00,
+        "descricao"=>"Bordados de luxo e uma paleta suave trazem uma vibe descolada ao AJ1",
+        "pecasEmEstoque" => 1,
+        "img" => "../img_produtos/air_jordan.jpg",
+        "marca" => "nike"
+    ],
+    [
+        "nome" => "all star original topssimo",
+        "preco"=> 200.00,
+        "desconto" => 160.00,
+        "descricao"=>"O clássico Chuck Taylor All Star transformado com elementos utilitários e ainda mais estilo.        ",
+        "pecasEmEstoque" => 1,
+        "img" => "../img_produtos/allstar_branco.jpg",
+        "marca" => "converse"
+    ],
+    [
+        "nome" => "Sandália Salto Alto Chá de Mel 36",
+        "preco"=> 60.00,
+        "desconto" => false,
+        "descricao"=>"pouco uso",
+        "pecasEmEstoque" => 1,
+        "img" => "../img_produtos/chademel.jpg",
+        "marca" => "chá de mel"
+    ],
+    [
+        "nome" => "Bota Branca Salto Transparente",
+        "preco"=> 150.00,
+        "desconto" => false,
+        "descricao"=>"usado apenas 2 vezes",
+        "pecasEmEstoque" => 1,
+        "img" => "../img_produtos/botabranca.jpg",
+        "marca" => "chá de mel"
+    ],
+    [
+        "nome" => "Sandália Nude Meia Pata",
+        "preco"=> 65.00,
+        "desconto" => 57.00,
+        "descricao"=>"semi-nova, usada apenas 3 vezes",
+        "pecasEmEstoque" => 1,
+        "img" => "../img_produtos/meiapata.jpg",
+        "marca" => "chá de mel"
+    ],
 ]
 ?>
